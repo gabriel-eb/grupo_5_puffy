ebbcbe1eb937f09d3bbf208f77e0d5eb97137485
gabriel-eb <gabo_espburg@hotmail.com>
2021-06-06 13:36:21 -0500

    Creacion del header y app

---
+++ Puffy/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const { join } = require("path");
+const app = express();
+const path = require("path");
+const PORT = 3030;
+
+app.use(express.static(path.join(__dirname, "/public")));
+
+app.get("/", (req, res) => {
+    res.status(200).sendFile("home.html", { root: "views" });
+});
+
+app.listen(PORT, () => {
+    console.log("Escuchando en http://localhost:" + PORT + "/");
+});
