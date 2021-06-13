482ae8ed80628985f53389338a1211b708bb6003
esteban1810 <estebansevillapg@gmail.com>
2021-06-13 11:29:06 -0500

    Terminado

--- Puffy/app.js
+++ Puffy/app.js
@@ -10,6 +10,14 @@ app.get("/", (req, res) => {
     res.status(200).sendFile("home.html", { root: "views" });
 });
 
+app.get("/detalle", (req, res) => {
+    res.status(200).sendFile("detalle.html", { root: "views" });
+});
+
+app.get("/carrito", (req, res) => {
+    res.status(200).sendFile("carrito.html", { root: "views" });
+});
+
 app.listen(PORT, () => {
     console.log("Escuchando en http://localhost:" + PORT + "/");
 });
