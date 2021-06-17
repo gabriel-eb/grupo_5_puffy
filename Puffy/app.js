e435c94a35be28436979d9345ce9784a4421a939
esteban1810 <estebansevillapg@gmail.com>
2021-06-16 20:58:18 -0500

    merge con montse

--- Puffy/app.js
+++ Puffy/app.js
@@ -17,6 +17,14 @@ app.get("/carrito", (req, res) => {
     res.status(200).sendFile("carrito.html", { root: "views" });
 });
 
+app.get("/login", (req, res) => {
+    res.status(200).sendFile("formulario_login.html", { root: "views" });
+});
+
+app.get("/signin", (req, res) => {
+    res.status(200).sendFile("formulario_signin.html", { root: "views" });
+});
+
 app.listen(PORT, () => {
     console.log("Escuchando en http://localhost:" + PORT + "/");
 });
