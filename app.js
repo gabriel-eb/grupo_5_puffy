d351218b6d8a3799a2f9c1c41fc124ada7669c9f
gabriel-eb <gabo_espburg@hotmail.com>
2021-06-16 22:13:49 -0500

    Configuracion correcta del puerto

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-const PORT = 3030;
+const PORT = process.env.PORT || 80;
 
 app.use(express.static(path.join(__dirname, "/public")));
 
