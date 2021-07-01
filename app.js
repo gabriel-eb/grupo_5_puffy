8ac1dd7e537fbb05b3d0572725e3040a12102192
gabriel-eb <gabo_espburg@hotmail.com>
2021-06-30 20:17:57 -0500

    Port y logo de header corredigos

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
 const express = require("express");
 const app = express();
 const path = require("path");
-<<<<<<< HEAD:app.js
-const PORT = process.env.PORT || 80;
-=======
 const rutaMain = require("./routes/main");
 const rutaProducts = require("./routes/products");
 const rutaUsers = require("./routes/users");
 const PORT = process.env.PORT || 3030;
->>>>>>> sprint3:Puffy/app.js
 
 // app.set("views", __dirname + '/carpetaViews');
 app.set("view engine", "ejs");
