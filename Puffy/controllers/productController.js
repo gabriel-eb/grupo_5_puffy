12b8d4e8121a4c3ef26fbe698121c96591bd8a7f
gabriel-eb <gabo_espburg@hotmail.com>
2021-09-23 18:45:01 -0500

    Vista de detalle del producto actualizada

--- Puffy/controllers/productController.js
+++ Puffy/controllers/productController.js
@@ -59,30 +59,38 @@ const controller = {
     },
     detalle: async (req, res) => {
         try {
-            let productDetail = await Products.findOne({
-                where: {
-                    id: req.params.id
-                }
-            });
-            productDetail = { ...productDetail.currentValues }
+            const productDetail = await Products.findByPk(req.params.id);
             
-            let category = await ProductCat.findOne({
+            const category = await ProductCat.findOne({
                 where: {
                     productId: req.params.id
                 },
-                include: ['category']
+                include: [{
+                    model: Categories, 
+                    as: 'category',
+                    attributes: ['name']
+                }]
             });
-            category = JSON.stringify(category);
-            category = JSON.parse(category);
-            category = category.category.name;
             
             const image = await Images.findOne({
                 where: {
-                    productId: req.params.id
+                    productId: req.params.id,
+                    main: true
                 }
             });
+            
+            if (req.session.userId){
+                const user = await db.User.findByPk(req.session.userId);
+                const userIsAdmin = user.admin;
+                return res.status(200).render("products/detalle", {
+                    productDetail,
+                    category,
+                    image,
+                    userIsAdmin
+                });
+            }
 
-            res.status(200).render("products/detalle", { productDetail, category, image });
+            return res.status(200).render("products/detalle", { productDetail, category, image });
 
         } catch (error) {
             console.log(error);
