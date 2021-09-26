8476f5aea934730a0a20f2532a10d2d401b494a7
gabriel-eb <gabo_espburg@hotmail.com>
2021-09-26 18:14:21 -0500

    Agregando bloques try-catch a index, search y getHighlight

--- Puffy/controllers/mainController.js
+++ Puffy/controllers/mainController.js
@@ -8,64 +8,78 @@ const Products = db.Product;
 const Images = db.Product_images;
 
 async function getHighlight() {
-    // Postre del dia
-    const maxQuant = await Products.max('quantity');
-    const highlight = await Products.findOne({
-        where: {
-            quantity: maxQuant
-        },
-        include: [{ model: Images, as: 'product_images' }]
-    });
+    try {
+        // Postre del dia
+        const maxQuant = await Products.max('quantity');
+        const highlight = await Products.findOne({
+            where: {
+                quantity: maxQuant
+            },
+            include: [{ model: Images, as: 'product_images' }]
+        });
+        // Creando objeto limpio y con url de imagen
+        const urlHighlight = highlight.product_images
+            .filter(img => img.main)[0].url;
 
-    // Creando objeto limpio y con url de imagen
-    const urlHighlight = highlight.product_images
-        .filter(img => img.main)[0].url;
-
-    return Object.assign(
-        {},
-        {
-            id: highlight.id,
-            name: highlight.name,
-            price: parseFloat(highlight.price),
-            discount: highlight.discount,
-            image: urlHighlight
-        }
-    );
+        return Object.assign(
+            {},
+            {
+                id: highlight.id,
+                name: highlight.name,
+                price: parseFloat(highlight.price),
+                discount: highlight.discount,
+                image: urlHighlight
+            }
+        );
+    } catch (error) {
+        console.log(error);
+        return null;
+    }
 }
 
 const controller = {
     index: async (req, res) => {
-        // Postre del dia
-        const highlight = await getHighlight();
+        try {
+            // Postre del dia
+            const highlight = await getHighlight();
 
-        // Postres destacados
-        let recentProducts = await Products.findAll({
-            where: {
-                quantity: { [Op.gt]: 0 }
-            },
-            order: [['updatedAt', 'DESC']],
-            limit: 4,
-            include: [{ model: Images, as: 'product_images' }]
-        });
-        // Limpiando obj y agregando URL de postres destacados
-        recentProducts = recentProducts.map(product => {
-            const urlProduct = product.product_images
-                .filter(img => img.main)[0].url;
-            return Object.assign(
-                {},
-                {
-                    id: product.id,
-                    name: product.name,
-                    price: product.price,
-                    discount: product.discount,
-                    image: urlProduct
-                }
-            );
-        });
+            // Postres destacados
+            let recentProducts = await Products.findAll({
+                where: {
+                    quantity: { [Op.gt]: 0 }
+                },
+                order: [['updatedAt', 'DESC']],
+                limit: 4,
+                include: [{ model: Images, as: 'product_images' }]
+            });
 
-        const categories = await db.Category.findAll();
 
-        res.status(200).render("index", { highlight, recentProducts, categories });
+            // Limpiando obj y agregando URL de postres destacados
+            recentProducts = recentProducts.map(product => {
+                const urlProduct = product.product_images
+                    .filter(img => img.main)[0].url;
+                return Object.assign(
+                    {},
+                    {
+                        id: product.id,
+                        name: product.name,
+                        price: product.price,
+                        discount: product.discount,
+                        image: urlProduct
+                    }
+                );
+            });
+            const categories = await db.Category.findAll();
+
+            return res.status(200).render("index", {
+                highlight,
+                recentProducts,
+                categories
+            });
+        } catch (error) {
+            console.log(error)
+            return res.status(500).render("error");
+        }
     },
     carrito: (req, res) => {
         res.status(200).render("carrito");
@@ -122,7 +136,8 @@ const controller = {
             await Users.create(userToCreate);
             return res.status(201).redirect('/login');
         } catch (error) {
-            return res.status(500).json({ error: error.message })
+            console.log(error)
+            return res.status(500).render("error");
         }
     },
     //Proceso Login
@@ -218,49 +233,54 @@ const controller = {
 
         if (!req.query.q) {
             delete query.where.name;
-        } 
+        }
         if (!req.query.cat) {
             query.include.pop();
         }
 
-        let searchResult = await Products.findAll(query);
+        try {
+            let searchResult = await Products.findAll(query);
 
-        const categoryName = searchResult.length && searchResult[0].product_category ?
-            searchResult[0].product_category[0].category.name : null;
+            const categoryName = searchResult.length && searchResult[0].product_category ?
+                searchResult[0].product_category[0].category.name : null;
 
 
-        // Limpiando obj y agregando URL de postres destacados
-        searchResult = searchResult.map((product) => {
-            const urlProduct = product.product_images
-                .filter(img => img.main)[0].url;
-            return Object.assign(
-                {},
-                {
-                    id: product.id,
-                    name: product.name,
-                    price: product.price,
-                    discount: product.discount,
-                    image: urlProduct
-                }
-            );
-        });
-
-        const categories = await db.Category.findAll({ raw: true });
-
-        if (searchResult.length === 0) {
-            const highlight = await getHighlight();
-            return res.status(200).render('search', { highlight, categories });
-        }
-
-        if (categoryName) {
-            return res.status(200).render('search', { 
-                searchResult, 
-                categoryName,
-                categories
+            // Limpiando obj y agregando URL de postres destacados
+            searchResult = searchResult.map((product) => {
+                const urlProduct = product.product_images
+                    .filter(img => img.main)[0].url;
+                return Object.assign(
+                    {},
+                    {
+                        id: product.id,
+                        name: product.name,
+                        price: product.price,
+                        discount: product.discount,
+                        image: urlProduct
+                    }
+                );
             });
-        }
 
-        return res.status(200).render('search', { searchResult, categories });
+            const categories = await db.Category.findAll({ raw: true });
+
+            if (searchResult.length === 0) {
+                const highlight = await getHighlight();
+                return res.status(200).render('search', { highlight, categories });
+            }
+
+            if (categoryName) {
+                return res.status(200).render('search', {
+                    searchResult,
+                    categoryName,
+                    categories
+                });
+            }
+
+            return res.status(200).render('search', { searchResult, categories });
+        } catch (error) {
+            console.log(error);
+            return res.status(500).render("error");
+        }
     }
 
 };
