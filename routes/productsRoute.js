4efe49b9ac27c9a8db056d41e1a257ae83aaa424
gabriel-eb <gabo_espburg@hotmail.com>
2021-09-27 22:12:21 -0500

    Arreglando conflicto en producsRoute.js

--- routes/productsRoute.js
+++ routes/productsRoute.js
@@ -9,13 +9,6 @@ const validator = require('../middlewares/validateProductMiddleware');
 const validatorEdit = require('../middlewares/validateEditProductMiddleware');
 
 router.get("/", controller.index);
-<<<<<<< HEAD:routes/productsRoute.js
-router.route("/agregar").get(notLogged,controller.vistaAgregar)
-.post(uploadFile.single('image'), gcpImage, controller.agregar);
-router.get("/:id", controller.detalle);
-router.get('/delete/:id', notLogged, controller.delete);
-router.route("/modificar/:id").get(notLogged, controller.vistaModificar).put(uploadFile.single('image'), gcpImage,controller.modificar);
-=======
 router.route("/agregar")
     .get(notLogged,controller.vistaAgregar)
     .post(uploadFile.single('image'), gcpImage, validator, controller.agregar);
@@ -24,6 +17,5 @@ router.get('/:id/delete', notLogged, controller.delete);
 router.route("/:id/edit")
     .get(notLogged, controller.vistaModificar)
     .put(uploadFile.single('image'), gcpImage, validatorEdit, controller.modificar);
->>>>>>> sprint7:Puffy/routes/productsRoute.js
 
 module.exports = router;
