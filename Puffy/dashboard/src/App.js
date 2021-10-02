11b14c9ef261ed448d902197ed3988c805c7a5b3
gabriel-eb <gabo_espburg@hotmail.com>
2021-10-02 14:57:39 -0500

    Agregando url de proxy y ejemplo de consumo de API en React

--- Puffy/dashboard/src/App.js
+++ Puffy/dashboard/src/App.js
@@ -1,9 +1,33 @@
+import { useState, useEffect } from 'react';
 import './App.css';
 
+
+
 function App() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('/api/products')
+      .then(res => res.json())
+      .then(res => setProducts(res.products))
+  }, []);
+
+  const ejemplo = () => {
+    return products.map(prod => {
+      return (
+        <>
+          <p>{prod.name}</p>
+          <p>{prod.description}</p>
+        </>
+      )
+    })
+  }
+
   return (
     <div className="App">
       <h1>Hello!</h1>
+      {ejemplo()}
     </div>
   );
 }
