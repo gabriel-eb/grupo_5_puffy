f1cd39d552339872be506ff4504a7cb06a122ca3
gabriel-eb <gabo_espburg@hotmail.com>
2021-10-09 13:28:09 -0500

    Preuba react-router-dom 2

--- dashboard/src/PruebaRouter.js
+++ dashboard/src/PruebaRouter.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, NavLink, Redirect } from 'react-router-dom';
+import { Route, NavLink, Redirect, Switch } from 'react-router-dom';
 import App from './App';
 import Page from './Page';
 
