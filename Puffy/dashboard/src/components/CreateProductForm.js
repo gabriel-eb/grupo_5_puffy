dd4d2323632d9fdedb5339a770cf09e57be1557f
gabriel-eb <gabo_espburg@hotmail.com>
2021-10-12 01:49:30 -0500

    Agregando Bootstrap a index react y poniendo estilo en form de crear prod

--- Puffy/dashboard/src/components/CreateProductForm.js
+++ Puffy/dashboard/src/components/CreateProductForm.js
@@ -19,7 +19,7 @@ function CreateProductForm() {
             .then(res => res.json())
             .then(res => setCategories(res))
             .catch(err => console.log(err));
-    },[]);
+    }, []);
 
     const handleSubmit = (e) => {
         e.preventDefault();
@@ -48,79 +48,129 @@ function CreateProductForm() {
     }
 
     return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label>Nombre:</label>
-                <input
-                    type="text"
-                    name="prodName"
-                    id="prodName"
-                    value={prodName}
-                    onChange={e => setProdName(e.target.value)}
-                    required
-                />
-                <label>Descripción:</label>
-                <textarea
-                    id="description"
-                    name="description"
-                    value={description}
-                    onChange={e => setDescription(e.target.value)}
-                    required
-                ></textarea>
-                <label>Imagen del producto:</label>
-                <input 
-                    type="file" 
-                    name="image" 
-                    id="image"
-                    onChange={e => setImage(e.target.files[0]) }
-                    accept="image/png, image/jpeg, image/jpg" 
-                    required 
-                />
-                <label>Precio:</label>
-                <input 
-                    type="number" 
-                    name="price" 
-                    id="price"
-                    value={price}
-                    onChange={e => setPrice(e.target.value)}
-                    required
-                />
-                <label>Cantidad:</label>
-                <input
-                    type="number"
-                    name="quantity"
-                    id="quantity"
-                    value={quantity}
-                    onChange={e => setQuantity(e.target.value)}
-                    required
-                />
-                <label>Categoría:</label>
-                <select 
-                    name="category" 
-                    id="category"
-                    value={choosedCat}
-                    onChange={e => setChoosedCat(e.target.value)}
-                    required
-                >
-                <option value=""></option>
-                { categories.map(category => <option value={category.id}>{category.name}</option>) }
-                </select>
-                <label>Tamaño: </label>
-                <select 
-                    name="size" 
-                    id="size"
-                    value={size}
-                    onChange={e => setSize(e.target.value)}
-                    required
-                >
-                    <option value=""></option>
-                    <option value="0">Chico</option>
-                    <option value="1">Mediano</option>
-                    <option value="2">Grande</option>
-                </select>
-                {!processing && <button> Crear </button>}
-                {processing && <button disabled> Creando... </button>}
-            </form>
+        <div className="row justify-content-md-center">
+            <div className="col-12 px-5 px-md-0 col-md-10">
+                <h2 className="mt-3">Crear Producto</h2>
+                <form onSubmit={handleSubmit}>
+                    <div className="row">
+                        <div className="col-md-8 col-12">
+                            <div className="form-floating mb-3">
+                                <input
+                                    type="text"
+                                    className="form-control"
+                                    placeholder="Nombre"
+                                    name="prodName"
+                                    id="prodName"
+                                    value={prodName}
+                                    onChange={e => setProdName(e.target.value)}
+                                    required
+                                />
+                                <label>Nombre:</label>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <textarea
+                                    id="description"
+                                    name="description"
+                                    className="form-control"
+                                    placeholder="Descripción"
+                                    value={description}
+                                    onChange={e => setDescription(e.target.value)}
+                                    required
+                                ></textarea>
+                                <label>Descripción:</label>
+                            </div>
+                            <div className="mb-3">
+                                <label className="float-start mb-1">Imagen del producto:</label>
+                                <input
+                                    type="file"
+                                    name="image"
+                                    id="image"
+                                    className="form-control"
+                                    onChange={e => setImage(e.target.files[0])}
+                                    accept="image/png, image/jpeg, image/jpg"
+                                    required
+                                />
+                            </div>
+                        </div>
+                        <div className="col-md-4 col-12">
+                        <div className="row">
+                            <div className="form-floating mb-3 col-md-6">
+                                <input
+                                    type="number"
+                                    name="price"
+                                    id="price"
+                                    className="form-control"
+                                    placeholder="Precio"
+                                    value={price}
+                                    onChange={e => setPrice(e.target.value)}
+                                    required
+                                />
+                                <label>Precio:</label>
+                            </div>
+                            <div className="form-floating mb-3 col-md-6">
+                                <input
+                                    type="number"
+                                    name="quantity"
+                                    id="quantity"
+                                    className="form-control"
+                                    placeholder="Cantidad"
+                                    value={quantity}
+                                    onChange={e => setQuantity(e.target.value)}
+                                    required
+                                />
+                                <label>Cantidad:</label>
+                            </div>
+                            </div>
+                            <div className="form-floating mb-3">
+                                <select
+                                    name="category"
+                                    id="category"
+                                    className="form-select"
+                                    placeholder="Categoria"
+                                    value={choosedCat}
+                                    onChange={e => setChoosedCat(e.target.value)}
+                                    required
+                                >
+                                    <option value=""></option>
+                                    {categories.map(category => <option value={category.id}>{category.name}</option>)}
+                                </select>
+                                <label>Categoría:</label>
+                            </div>
+                            <div className="form-floating mb-3 mt-md-4">
+                                <select
+                                    name="size"
+                                    id="size"
+                                    className="form-select"
+                                    placeholder="Tamaño"
+                                    value={size}
+                                    onChange={e => setSize(e.target.value)}
+                                    required
+                                >
+                                    <option value=""></option>
+                                    <option value="0">Chico</option>
+                                    <option value="1">Mediano</option>
+                                    <option value="2">Grande</option>
+                                </select>
+                                <label>Tamaño: </label>
+                            </div>
+                        </div>
+                        <div>
+                            {
+                                !processing && 
+                                <button className="btn btn-success mt-2 btn-lg col-12 col-md-2 offset-md-10"> 
+                                Crear 
+                                </button>
+                            }
+                            {
+                                processing && 
+                                <button className="btn btn-secondary mt-2 btn-lg col-12 col-md-2 offset-md-10" disabled>
+                                 Creando... 
+                                </button>
+                            }
+                        </div>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
