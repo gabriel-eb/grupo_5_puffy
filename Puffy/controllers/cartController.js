862cf7dc00a04d6337d5235c2212b88f7051c46c
gabriel-eb <gabo_espburg@hotmail.com>
2021-12-22 11:25:31 -0600

    Agregando form para invitado en checkout del carrito

--- Puffy/controllers/cartController.js
+++ Puffy/controllers/cartController.js
@@ -1,10 +1,11 @@
 const { v4: uuidv4 } = require('uuid');
+const { Op } = require("sequelize");
 const db = require('../database/models');
+const Users = db.User;
 const Products = db.Product;
 const Carts = db.Cart;
-const Address = db.Address;
-const ProdCarts = db.ProductCart;
 const Images = db.ProductImages;
+const Invited = db.Invited;
 
 
 const controller = {
@@ -13,9 +14,9 @@ const controller = {
         const cart = req.session.cart;
         if (userCart && cart && cart.length > 0) {
             const userType = req.session.userId ? 1 : 0;
-            return res.render('carrito', { userCart, userType, cart });
+            return res.render('cart/carrito', { userCart, userType, cart });
         }
-        return res.render('emptyCart');
+        return res.render('cart/emptyCart');
     },
     addProduct: async (req, res) => {
         try {
@@ -94,6 +95,48 @@ const controller = {
             return res.status(500);
         }
     },
+    invitedCheckout: (req, res) => {
+        const estados = [
+            'Aguascalientes', 'Baja California', 'Baja California Sur',
+            'Campeche', 'Coahuila de Zaragoza', 'Colima', 'Chiapas',
+            'Chihuahua', 'Ciudad de México', 'Durango', 'Guanajuato',
+            'Guerrero', 'Hidalgo', 'Jalisco', 'Estado de México',
+            'Michoacán de Ocampo', 'Morelos', 'Nayarit', 'Nuevo León',
+            'Oaxaca de Juárez', 'Puebla', 'Querétaro', 'Quintana Roo',
+            'San Luis Potosí', 'Sinaloa', 'Sonora', 'Tabasco',
+            'Tamaulipas', 'Tlaxcala', 'Veracruz de Ignacio de la Llave',
+            'Yucatán', 'Zacatecas'
+        ]
+        return res.status(200).render('cart/invitedCheckout', { estados });
+    },
+    checkEmail: async (req, res) => {
+        try {
+            const user = await Users.findOne({
+                where: {
+                    email: {
+                        [Op.like]: req.body.email
+                    }
+                }
+            });
+            if(user){
+                return res.status(200).send({exists:true});
+            } else {
+                return res.status(200).send({ exists: false });
+            }
+        } catch (error) {
+            console.log(error);
+            return res.status(500);
+        }
+    },
+    createInvited: async (req, res) => {
+        try {
+            Invited.create(req.body);
+            return res.status(201);
+        } catch (error) {
+            console.log(error);
+            return res.status(500);
+        }
+    },
     buyCart: async (req, res) => {
 
         /**
@@ -120,7 +163,7 @@ const controller = {
                 );
             }
 
-            return res.status(201).render('agradecimiento');
+            return res.status(201).render('cart/agradecimiento');
         } catch (error) {
             console.log(error);
             return res.status(500);
--- Puffy/routes/cartsRoute.js
+++ Puffy/routes/cartsRoute.js
@@ -4,8 +4,11 @@ const controller = require("../controllers/cartController");
 
 router.get('/', controller.index);
 router.get('/checkout', controller.startCheckout);
+router.get('/invited', controller.invitedCheckout);
 router.get('/increase/:prodId', controller.increase);
 router.get('/decrease/:prodId', controller.decrease);
 router.post('/addProduct', controller.addProduct);
+router.post('/checkEmail', controller.checkEmail);
+router.post('/createInvited', controller.createInvited);
 
 module.exports = router;
